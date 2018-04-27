ca2907b6ff3b279e3ef347df04fcf54ac8f2463b
Lucimux <vicente.borja@alumno.buap.mx>
2018-04-26 22:54:56 -0500

    sistema experto partes acopladas :v

--- Diagnostico.py
+++ Diagnostico.py
@@ -1,27 +1,49 @@
 import re
 from pyknow import *
-from SistemaExperto import DatosPaciente, Dieta
 import csv
 
+class DatosPaciente(Fact):
+    pass
+
+class Dieta(Fact):
+    pass
+    
 class DiagnosTicoEngine(KnowledgeEngine):
         
     def GenerarDatos(self):
         with open('Datos.csv') as archivoCsv:
             datos = csv.reader(archivoCsv)
+            next(datos, None)
             for dato in datos:
-                sexo, peso, edad, estatura, cintura, cadera, perimetroAbd, circMun, porPesoT, pesoHabit, ICC, complexion, antecedentes, ejercicio = dato
-                self.declare(DatosPaciente(sexo=sexo,peso= peso, edad=edad, estatura=estatura, cintura=cintura, cadera=cadera, perimetroAbd=perimetroAbd, circunfMun=circMun, porPesoTeorico=porPesoT, porPesoHabit=pesoHabit, icc=ICC, complexion=complexion, antecedentes=antecedentes, ejercicio=ejercicio))
-
-    @Rule(DatosPaciente(sexo=MATCH.sexo & P(lambda sexo: re.match(r"\d+x\d+", sexo)),porPesoHabit=MATCH.porPesoHabit & P(lambda peso: re.match(r"\d+x\d+", porPesoHabit))))
-    def wacharDieta(self,sexo,porPesoHabit):
+                sexo, peso, edad, estatura, cintura, cadera, pesoTeoIdeal, circMun, porPesoT, ICC, pesoHabit, complexion, antecedentes, ejercicio = dato
+                self.declare(DatosPaciente(sexo=sexo,peso=str(peso), edad=str(edad), estatura=str(estatura), cintura=str(cintura),cadera=str(cadera), 
+                pesoTeoIdeal=str(pesoTeoIdeal), circunfMun=str(circMun), porPesoTeorico=str(porPesoT), 
+                icc=str(ICC), porPesoHabit=str(pesoHabit), complexion=str(complexion), antecedentes=str(antecedentes), ejercicio=str(ejercicio)))
+    @Rule(DatosPaciente(
+            sexo=MATCH.sexo & L('masculino')
+            #peso=MATCH.peso & P(lambda peso: re.match(/^masculino$/, peso)),
+            #edad=MATCH.edad & P(lambda edad: re.match(r"\d+x\d+", edad)),
+            #estatura=MATCH.estatura & P(lambda estatura: re.match(r"\d+x\d+", estatura)),
+            #cintura=MATCH.cintura & P(lambda cintura: re.match(r"\d+x\d+", cintura)),
+            #cadera=MATCH.cadera & P(lambda cadera: re.match(r"\d+x\d+", cadera)),
+            #pesoTeoIdeal=MATCH.pesoTeoIdeal & P(lambda pesoTeoIdeal: re.match(r"\d+x\d+", pesoTeoIdeal)),
+            #circunfMun=MATCH.circunfMun & P(lambda circunf: re.match(r"\d+x\d+", circunfMun)),
+            #porPesoTeorico=MATCH.ppt & P(lambda ppt: re.match(r"\d+x\d+", ppt)),
+            #icc=MATCH.icc & P(lambda icc: re.match(r"\d+x\d+", icc)),
+            #porPesoHabit=MATCH.pph & P(lambda pph: re.match(r"\d+x\d+", pph)),            
+            #complexion=MATCH.complexion & P(lambda complexion: re.match(r"\d+x\d+", complexion)),
+            #antecedentes=MATCH.antecedentes & P(lambda antecedentes: re.match(r"\d+x\d+", antecedentes)),
+            #ejercicio=MATCH.ejercicio & P(lambda sexo: re.match(r"\d+x\d+", ejercicio)),        
+        ))
+    def DietaHombres(self,sexo):
+        #peso,edad,estatura,cintura,cadera,pesoTeoIdeal,circunfMun,ppt,icc,pph,complexion,antecedentes,ejercicio):
         #print(sexo,porPesoHabit)
         print("Hechate otro pozolito marrano :v")
         self.declare(Dieta(carnes="Ni de pedo"))
-
-watch('RULES','FACTS')
-algo = DiagnosTicoEngine()
-algo.reset()
-algo.GenerarDatos()
-algo.wacharDieta('masculino','Sin Denutricion')
-algo.run()
-print(algo.facts)
+    @Rule(DatosPaciente(
+            sexo=MATCH.sexo & L('femenino')))
+    
+    def DietaMujeres(self,sexo):
+        print("Hechate otros taquitos marrana :v")
+        self.declare(Dieta(carnes="Pura lechuguita marrana"))
+        
--- Gui.pyw
+++ Gui.pyw
@@ -1,12 +1,8 @@
 import sys, re
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton
-#fuentes
-from PyQt5.QtGui import QFont
-#cambiar cursor
-from PyQt5.QtCore import Qt
 from PyQt5 import uic
 from Paciente import Paciente
-
+from Diagnostico import DiagnosTicoEngine, DatosPaciente
 class SistemaExpertoGUI(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
@@ -34,13 +30,13 @@ class SistemaExpertoGUI(QMainWindow):
             return True
     def generoValue(self):
         if self.masculino.isChecked():
-            return "M"
+            return "masculino"
         elif self.femenino.isChecked():
-            return "F"
+            return "femenino"
     
     def getDieta(self):
         nombre = self.nombre.text()
-        edad = self.edad.text()
+        edad = int(self.edad.text())
         estatura = float(self.estatura.text())
         cintura = float(self.cintura.text())
         cadera = float(self.cadera.text())        
@@ -50,7 +46,16 @@ class SistemaExpertoGUI(QMainWindow):
         circunferencia_mun = float(self.circunferencia_mun.text())
         sexo = self.generoValue()
         paciente = Paciente(nombre,sexo,edad,estatura,cintura,cadera,peso_actual,peso_habitual,perimetro_abdominal,circunferencia_mun)
-        print(paciente)
+        #print(paciente)
+        algo = DiagnosTicoEngine()
+        algo.reset()
+        algo.GenerarDatos()
+        #algo.declare(DatosPaciente(sexo='masculino',peso='95',edad='25',estatura="1,88",cintura='99',cadera='110',pesoTeoIdeal="75,9896",cincunfMun='19',porPesoTeorico="125,0171076",icc="0,9",porPesoHabit="9,894736842",complexion='COMPLEXION MEDIANA',antecedentes='sobrepeso',ejercicio='no'))
+        algo.declare(DatosPaciente(sexo=paciente.sexo))
+        #algo.declare(DatosPaciente(sexo="femenino"))
+        algo.run()
+        print(algo.facts)
+
 
 
 app = QApplication(sys.argv)
